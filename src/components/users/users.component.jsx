e01e31096c1775e19f8159fc30b568e45272db36
Opeyemi Peter <opeyemiadebowale1759@gmail.com>
2021-01-11 14:40:17 -0800

    adding user image

--- src/components/users/users.component.jsx
+++ src/components/users/users.component.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import allUser from '../../assests/allUser.png';
+import femaleUser from '../../assests/femaleUser.png';
+import maleUser from '../../assests/maleUser.png';
+
 import './users.styles.css'
 
 
@@ -7,7 +11,7 @@ const UserPage =() =>{
   return(
   <div className="container">
       <div className="text-white header">
-        <p className='mt-3 heading-text'>Hello, <strong className="">Emerald</strong></p>
+        <p className='mt-3 heading-text'>Hello, <strong className="emerald">Emerald</strong></p>
         <p>Welcome to your dashboard. Kindly sort through the user base.</p>
         
       </div>
@@ -18,7 +22,29 @@ const UserPage =() =>{
         <input type="text" className="form-control col-md-5 text-white" placeholder="Find a user" disabled/>
       </div>
 
-      
+      <div className="show-users">
+        <h5 className="text-white pt-5 pb-5 mt-3">
+          Show Users
+        </h5>
+        <div className="row">
+        <div className="col-4-md mr-4">
+          <a href="#">
+         <img src={allUser} alt="" />
+          </a>
+        </div>
+        <div className="col-4-md mr-4">
+          <a href="#">
+         <img src={maleUser} alt="" />
+          </a>
+        </div>
+        <div className="col-4-md pb-5">
+          <a href="#">
+         <img src={femaleUser} className="mb-5" alt="" />
+          </a>
+        </div>
+        </div>
+        
+      </div>
 
   </div>
   )
